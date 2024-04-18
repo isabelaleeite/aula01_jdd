631c689f55f552c27fec0eca7340f04692bc7bb3
Isabela.leite <isabela.leeite@hotmail.com>
2024-04-17 23:39:41 -0300

    Resolução do exercício

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello Word")
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+valor01 = (int(input("Digite um valor: ")))
+valor02 = (int(input("Digite outro valor: ")))
+
+soma = valor01 + valor02
+
+print("A soma entre {} e {} é igual a {}".format(valor01, valor02, soma ))
+
